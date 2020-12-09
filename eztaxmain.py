470991e179ec8e547e7d8adad4ad1c5c21e20f2b
quietarn <75717719+quietarn@users.noreply.github.com>
2020-12-09 13:04:34 -0500

    Add files via upload

--- eztaxmain.py
+++ eztaxmain.py
@@ -21,21 +21,82 @@ class APP(Frame):
         self.master = master
         self.pack()
         self.createButtons()
+        self.mainloop()
+        
+    def reporting(self):
+        filename = asksaveasfilename(title="save as...", initialfile="tax.txt", filetypes=[("text File", "*.txt")])
+        f = open(filename, "w")
+        f.write("Hello " + self.FirstName.get() + " " + self.LastName.get() + '\n')
+        f.write("Your tax preview for year " + self.year.get() + " will look like this" + '\n')
+        f.write("Your income tax: $" + "   "*3 + str(self.incometax) + '\n')
+        f.write("Your free parking benefit is: $" + "   "*3 + str(self.fp) + '\n')
+        f.write("Your allowance benefit is: $" + "   "*3 + str(self.a) + '\n')
+        f.write("Your incurred meal deductive is: $" + "   "*3 + str(self.meal) + '\n')
+        f.write("Your Automobile deductive is: $" + "   "*3 + str(self.abT) + '\n')
+        f.write("Your total is: $" + "   "*3 + str(self.incometax + self.fp + self.a - self.meal - self.abT) + '\n')
+        f.close
     
     def donate(self):
-        messagebox.showinfo("Support us", "EzTax is solely ran by university students, if you enjoying using our work, help us get better through xxxxxxxx@email.com")
-    
-    def savepdf(self):
-        filename = asksaveasfilename(title="save as...", initialfile="tax.pdf", filetypes=[("PDF File", "*.pdf")])
+        messagebox.showinfo("Support us", "EzTax is solely ran by university students, if you enjoying using our work, help us get better through EMT EZtax@gmail.com")
+        
+    def cra(self):
+        messagebox.showinfo("CRA", "Please contact CRA for further infomation, through\n" + "https://www.canada.ca/en/revenue-agency.html")
     
     def callback(self):
         self.abA, self.abB, self.abC, self.abD, self.abE, self.abF = float(self.abTA.get()), float(self.abTB.get()), float(self.abTC.get()), float(self.abTD.get()), float(self.abTE.get()), float(self.abTF.get())
-        print(self.abA, self.abB, self.abC, self.abD, self.abE, self.abF)
     
     def taxpre(self):
-        pass
+        top1 = Toplevel()
+        top1.title("Tax preview")
+        top1.geometry("700x700")
+        Label(top1, text=("Hello " + self.FirstName.get() + " " + self.LastName.get())).pack()
+        Label(top1, text=("Your tax preview for year " + self.year.get() + " will look like this")).pack()
+        Label(top1, text=" ").pack()
         
+        self.incometax = 0
+        if (float(self.income.get()) > 220000):
+            self.incometax = float(self.income.get()) * 0.1316
+        elif(float(self.income.get()) > 150000):
+            self.incometax = float(self.income.get()) * 0.1216
+        elif(float(self.income.get()) > 89482):
+            self.incometax = float(self.income.get()) * 0.1116
+        elif(float(self.income.get()) > 44740):
+            self.incometax = float(self.income.get()) * 0.0915
+        elif(float(self.income.get()) > 0):
+            self.incometax = float(self.income.get()) * 0.0505
+        Label(top1, text=("Your income tax: $" + "   "*3 + str(self.incometax))).pack()
+        Label(top1, text=" ").pack()
+        
+        if self.FreeParking.get():
+            self.fp = float(self.fp.get())
+            Label(top1, text=("Your free parking benefit is: $" + "   "*3 + str(self.fp))).pack()
+            Label(top1, text=" ").pack()
+            
+        if  self.a.get() != "":
+            self.a = float(self.a.get())
+            Label(top1, text=("Your allowance benefit is: $" + "   "*3 + str(self.a))).pack()
+            Label(top1, text=" ").pack()
+        
+        if self.meal.get() != "":
+            self.meal = float(self.meal.get()) * 0.5
+            Label(top1, text=("Your incurred meal deductive is: $" + "   "*3 + str(self.meal))).pack()
+            Label(top1, text=" ").pack()
+            
+        if self.ab.get():
+            if self.abF != 0:
+                self.abT = (self.abA / self.abB * (0.02 * (self.abC)) + 2 / 3 * (self.abE / self.abF))
+            else:
+                self.abT = self.abA / self.abB * (0.02 * (self.abC))
+            Label(top1, text=("Your Automobile deductive is: $" + "   "*3 + str(self.abT))).pack()
+            Label(top1, text=" ").pack()
+        
+        Label(top1, text=("Your total is: $" + "   "*3 + str(self.incometax + self.fp + self.a - self.meal - self.abT))).pack()
+        Label(top1, text="Thank you for using EzTax").pack()
+        
+        Button(top1, text="generate report", command=self.reporting).pack()
+
     def AutoMobile(self):
+        self.ab.set(True)
         top = Toplevel()
         top.title("Automobile")
         top.geometry("700x700")
@@ -61,7 +122,6 @@ class APP(Frame):
         self.abTF.insert(END, 0)
         
         Button(top, text="submit", command=self.callback).pack()
-        top.mainloop()
         
     def createButtons(self):
         """first name entry"""
@@ -84,6 +144,9 @@ class APP(Frame):
         self.SIN = Entry(self)
         self.SIN.grid(row=2, column=1)
         
+        """CRA"""
+        Button(self, text="Contact CRA", command=self.cra).grid(row=2, column=3)
+        
         """Address"""
         Label(self, text="Your Address:").grid(row=3, column=0, sticky="W")
         self.Address = Entry(self)
@@ -91,8 +154,8 @@ class APP(Frame):
         
         """year"""
         Label(self, text="Year applied:").grid(row=4, column=0, sticky="W")
-        self.Address = Entry(self)
-        self.Address.grid(row=4, column=1)
+        self.year = Entry(self)
+        self.year.grid(row=4, column=1)
         
         """student"""
         Label(self, text="Are you a student:").grid(row=5, column=0, sticky="W")
@@ -134,9 +197,7 @@ class APP(Frame):
         self.meal = Entry(self)
         self.meal.grid(row=10, column=1)
         
-        Button(self, text="generate report", command=self.taxpre).grid(row=11, column=1)
-        
-        self.mainloop()
+        Button(self, text="tax calculating", command=self.taxpre).grid(row=11, column=1)
         
         
 root = Tk()
